419c29596c2937c87f669964ce8cfbb627a88fea
Ahnsj828 <ala828@naver.com>
2024-09-27 20:41:03 +0900

    slider

--- slide/slide-youtube/script.js
+++ slide/slide-youtube/script.js
@@ -1,6 +1,13 @@
 let currentNum = 0;
+
+let imgInit = function () {
+  if (currentNum != 0) {
+    document.getElementById("img" + currentNum).classList.remove("hide");
+  }
+};
+
 setInterval(function () {
-  // currentNum++;
+  imgInit();
   if (currentNum == 4) {
     currentNum = 1;
   } else {
@@ -8,4 +15,11 @@ setInterval(function () {
   }
   console.log(currentNum);
   document.getElementById("img" + currentNum).classList.add("hide");
+
+  let clear = setInterval(function () {
+    document
+      .getElementById("imgSet")
+      .prepend(document.getElementById("img" + currentNum));
+    clearInterval(clear);
+  }, 1000);
 }, 3000);
