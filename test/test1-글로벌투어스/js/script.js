abfa734b16b428aa97d90dffc4a23d358068b8c5
Ahnsj828 <ala828@naver.com>
2025-08-27 18:55:21 +0900

    test study-popup - html, css and js

--- test/test1-글로벌투어스/js/script.js
+++ test/test1-글로벌투어스/js/script.js
@@ -1,5 +1,5 @@
 $(function () {
-  // 메뉴
+  // < 메뉴 >
   $("nav>ul>li").mouseenter(function () {
     // 마우스 오버시
     $(this).children(".sub").stop().slideDown();
@@ -12,7 +12,7 @@ $(function () {
   });
   // mouseenter 대신 mouseover를 사용해도 되지만 / 쌤은 mouseenter와 mouseleave 를 선호
 
-  // 슬라이드
+  // < 슬라이드 >
   // 슬라이드 요소 li 세개가 있다. li들을 움직이는게 아니라 부모인 ul 슬라이드 띠를 움직여야 한다.
   // $(".slide ul").animate({ marginLeft: -1200 }, 2000);
   // ul의 왼쪽 마진을 -1200px로 설정하여 첫번째 li가 보이지 않도록 한다.
@@ -49,4 +49,19 @@ $(function () {
   // setInterval(함수, 시간) => 주어진 시간마다 함수를 실행한다
   // 여기서는 3초마다 slide 함수를 실행하여 슬라이드가 자동으로 넘어가도록 한다.
   // 3초마다 slide 함수가 실행되면서 i가 증가하고, ul의 왼쪽 마진이 변경되어 슬라이드가 움직인다.
+
+  // < 팝업창 >
+  // .popup을 클릭했을 때 팝업창이 떠야된다.
+  $(".pop").click(function () {
+    $(".popup").show();
+    // .pop은 a태그라 클릭하면 다른 링크로 이동하는데 그걸 막아줘야 한다. 링크를 차단해줘야 한다.
+    return false;
+    // return false; => 클릭 이벤트의 기본 동작을 막아준다.
+  });
+
+  // .close 버튼을 클릭했을 때 팝업창이 닫혀야 된다.
+  $(".close button").click(function () {
+    $(".popup").hide();
+    // .hide() => 요소를 숨긴다.
+  });
 });
