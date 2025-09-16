92292e6c8c51f99a6e872dc038acf68c5b73e6cf
Ahnsj828 <ala828@naver.com>
2025-09-16 17:46:34 +0900

    B

--- test/글로벌투어스/js/script.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  $("nav>ul>li").mouseenter(function () {
-    $(this).children(".sub").stop().slideDown();
-  });
-  $("nav>ul>li").mouseleave(function () {
-    $(".sub").stop().slideUp();
-  });
-});
