ebadb304876dbff2d851ebcd6b13c47325839b3b
Ahnsj828 <ala828@naver.com>
2025-09-24 18:34:39 +0900

    test B study - header - html and css and javascript

--- test/B유형/올인원_세이프뱅크/js/script.js
+++ test/B유형/올인원_세이프뱅크/js/script.js
@@ -1,1 +1,13 @@
-$(function () {});
+$(function () {
+  // 메뉴
+  $("nav>ul>li").mouseover(function () {
+    // 마우스 오버시 - 이번엔 mouseenter이 아닌 mouseover를 썼다
+    $(".sub, .menuBg").stop().slideDown();
+    // 페이지 내 모든 메뉴(.sub)와 배경(.menuBg)이 같이 슬라이드 다운(열기)
+  });
+  $("nav>ul>li").mouseout(function () {
+    // 마우스 아웃시
+    $(".sub, .menuBg").stop().slideUp();
+    // 페이지 내 모든 메뉴(.sub)와 배경(.menuBg)이 같이 슬라이드 업(닫음)
+  });
+});
